5301586afb1e23996afc54a2707a690e209ec79a
vaghelarajal <rajal.v@bitontree.com>
2026-02-04 18:27:05 +0530

    Applied all changes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes.auth import router as auth_router
+import os
+from dotenv import load_dotenv
 
-# Simple, fast startup
+load_dotenv()
 app = FastAPI(title="Auth API", version="1.0.0")
 
-# Minimal CORS setup
+frontend_urls = os.getenv("FRONTEND_URLS", "").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=frontend_urls,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 
-# add authentication routes
 app.include_router(auth_router)
 
 
